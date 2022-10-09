e1060c5346e2a02d6f435cc81983b33d94f0685a
hotSMM <sun_dm@mail.ru>
2022-10-09 16:10:11 +0300

    Update Task_5.js

--- Task_5.js
+++ Task_5.js
@@ -5,3 +5,35 @@
 •	Имена классов, свойств и методов должны быть информативными;
 •	Соблюдать best practices;
 •	Использовать синтаксис ES6.
+
+class ElectricalDevice
+{
+  constructor(name, power)
+  {
+    this.name = name;
+    this.power = power;
+    this.isEnabled = false;
+  }
+  
+  plugIn()
+  {
+    console.log(this.name + " is work!");
+    this.isEnabled = true;
+  }
+  
+  getPowerUsed()
+  {
+    return this.isEnabled ? this.power : 0;
+  }
+}
+
+const radiator = new ElectricalDevice('radiator', 83)
+const iron = new ElectricalDevice('iron', 52);
+
+console.log(radiator.getPowerUsed() + iron.getPowerUsed());
+
+radiator.plugIn();
+console.log(radiator.getPowerUsed() + iron.getPowerUsed());
+
+iron.plugIn();
+console.log(radiator.getPowerUsed() + iron.getPowerUsed());
